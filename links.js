9c9fcc96b118cc8282b045fa1788f2809d61f52d
JovialKnoll <BrettCKaplan@gmail.com>
2017-05-14 20:47:12 -0400

    top nave setup

--- links.js
+++ links.js
@@ -3,22 +3,18 @@ var pages = [
     { link: "/roller", text: "Roller" },
     { link: "/about", text: "About" }
 ];
-
 var topNav = '<ul class="topnav">';
 for (var i = 0; i < pages.length; ++i) {
     var page = pages[i];
-    topNav += '<li><a href="';
+    topNav += '<li';
+    if (i === pages.length - 1) {
+        topNav += ' class="right"';
+    }
+    topNav += '><a href="';
     topNav += page.link === window.location.pathname
-        ? '#'
+        ? '#" class="active'
         : ('.' + page.link)
         ;
-    topNav += '" class="toplink';
-    if (page.link === window.location.pathname) {
-        topNav += ' active'
-    }
-    if (i === pages.length - 1) {
-        topNav += ' right'
-    }
     topNav += '">';
     topNav += page.text;
     topNav += '</a></li>';
