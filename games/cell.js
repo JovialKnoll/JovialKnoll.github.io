6293fc84c8af850a28eb5fecca42054c4e45a20c
JovialKnoll <BrettCKaplan@gmail.com>
2022-02-09 23:24:54 -0500

    no var for cell

--- games/cell.js
+++ games/cell.js
@@ -1,23 +1,16 @@
-// elements
-var aliveChanceButton = document.getElementById('alive-chance-button');
-var aliveChanceText = document.getElementById('alive-chance-text');
-var timeButton = document.getElementById('time-button');
-var timeText = document.getElementById('time-text');
-var canvas = document.getElementById('canvas');
-// state
-var size = 256;
-var deadBright = 48;
-var deadSteps = 6;
-var deadSpeed = deadBright / deadSteps;
-var redSpeed = 5;
-var blueSpeed = 4;
-var ctx = canvas.getContext('2d');
+const aliveChanceText = document.getElementById('alive-chance-text');
+const timeText = document.getElementById('time-text');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const size = 256;
+const redSpeed = 5;
+const blueSpeed = 4;
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, size, size);
-var nextTime;
-var born = [];
-var survives = [];
-var cellArray;
+let nextTime;
+let born = [];
+let survives = [];
+let cellArray;
 // make it work
 function getAliveChance() {
     return parseFloat(aliveChanceText.value);
@@ -46,9 +39,9 @@ function getNeighbor(i, j) {
     return cellArray[getWrappedIndex(i)][getWrappedIndex(j)] > 0 ? 1 : 0;
 }
 function getNewValue(i, j) {
-    var adjacentAliveCount = 0;
-    for (var x = -1; x < 2; ++x) {
-        for (var y = -1; y < 2; ++y) {
+    let adjacentAliveCount = 0;
+    for (let x = -1; x < 2; ++x) {
+        for (let y = -1; y < 2; ++y) {
             adjacentAliveCount += getNeighbor(i + x, j + y);
         }
     }
@@ -69,20 +62,20 @@ function getNewValue(i, j) {
         ;
 }
 function getColor(val) {
-    var r = Math.max(0, 255 + redSpeed - val * redSpeed).toString();
-    var b = Math.min(255, - blueSpeed + val * blueSpeed).toString();
-    var g = (255 - r - b);
+    const r = Math.max(0, 255 + redSpeed - val * redSpeed).toString();
+    const b = Math.min(255, - blueSpeed + val * blueSpeed).toString();
+    const g = (255 - r - b);
     return "rgb(" + r + "," + g + "," + b + ")";
 }
 function updateDrawArray() {
-    var newArray = [];
-    for (var i = 0; i < size; ++i)
+    const newArray = [];
+    for (let i = 0; i < size; ++i)
     {
-        var newColumn = [];
-        for (var j = 0; j < size; ++j)
+        const newColumn = [];
+        for (let j = 0; j < size; ++j)
         {
             newColumn.push(getNewValue(i, j));
-            var val = cellArray[i][j];
+            const val = cellArray[i][j];
             if (val > 0) {
                 ctx.fillStyle = getColor(val);
                 ctx.fillRect(i, j, 1, 1);
@@ -93,7 +86,7 @@ function updateDrawArray() {
     cellArray = newArray;
 }
 // looping and input
-var isRunning = false;
+let isRunning = false;
 function loop() {
     ctx.globalAlpha = 0.333;
     ctx.fillStyle = 'black';
@@ -112,12 +105,12 @@ setTime();
 function setRules() {
     born = [];
     survives = [];
-    for (var n = 0; n < 9; ++n) {
-        var bElement = document.getElementById('b' + n.toString());
+    for (let n = 0; n < 9; ++n) {
+        const bElement = document.getElementById('b' + n.toString());
         if (bElement.checked) {
             born.push(n);
         }
-        var sElement = document.getElementById('s' + n.toString());
+        const sElement = document.getElementById('s' + n.toString());
         if (sElement.checked) {
             survives.push(n);
         }
