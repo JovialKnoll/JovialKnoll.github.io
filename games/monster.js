dbd3a86991eb1606dfe87d76289836456c59f2ab
JovialKnoll <BrettCKaplan@gmail.com>
2022-02-09 19:45:44 -0500

    not using var

--- games/monster.js
+++ games/monster.js
@@ -1,8 +1,8 @@
 const smallSize = 48;
 const largeSize = 64;
-var canvasSize = 64;
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+const canvasSize = 64;
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'white';
 
 const levelSelect = document.getElementById('level');
@@ -25,32 +25,32 @@ const bodyGroups = [
     "B",
     "C"
 ];
-var images = {};
+const images = {};
 // setting up images
-for (var lvl = 0; lvl < 4; ++lvl) {
-    var parts = bodyParts;
+for (let lvl = 0; lvl < 4; ++lvl) {
+    let parts = bodyParts;
     if (lvl === 0) {
         parts = bodyParts.slice(1, 4);
     }
-    var size = lvl === 3
+    const size = lvl === 3
         ? largeSize
         : smallSize;
-    for (var i = 0; i < parts.length; ++i) {
-        var part = parts[i];
-        for (var j = 0; j < bodyGroups.length; ++j) {
-            var group = bodyGroups[j];
-            var fileStart = lvl.toString() + "-" + part + "-" + group;
+    for (let i = 0; i < parts.length; ++i) {
+        const part = parts[i];
+        for (let j = 0; j < bodyGroups.length; ++j) {
+            const group = bodyGroups[j];
+            const fileStart = lvl.toString() + "-" + part + "-" + group;
             if (lvl === 0) {
-                var file = fileStart + ".png";
-                var image = new Image(size, size);
+                const file = fileStart + ".png";
+                const image = new Image(size, size);
                 image.src = "./images/" + file;
                 images[fileStart] = image;
             }
             else {
-                for (var k = 0; k < 3; ++k) {
-                    var fileKey = fileStart + k.toString();
-                    var file = fileKey + ".png";
-                    var image = new Image(size, size);
+                for (let k = 0; k < 3; ++k) {
+                    const fileKey = fileStart + k.toString();
+                    const file = fileKey + ".png";
+                    const image = new Image(size, size);
                     image.src = "./images/" + file;
                     images[fileKey] = image;
                 }
@@ -63,31 +63,31 @@ function getLvl() {
 }
 function levelChange() {
     // empty out options
-    for (var i = 0; i < bodySelects.length; ++i) {
-        var bodySelect = bodySelects[i];
+    for (let i = 0; i < bodySelects.length; ++i) {
+        const bodySelect = bodySelects[i];
         bodySelect.length = 0;
         bodySelect.disabled = true;
     }
     const lvl = getLvl();
-    var selects = bodySelects;
+    let selects = bodySelects;
     if (lvl === 0) {
         selects = bodySelects.slice(1, 4);
     }
     // fill in options
-    for (var i = 0; i < selects.length; ++i) {
-        var select = selects[i];
-        for (var j = 0; j < bodyGroups.length; ++j) {
-            var group = bodyGroups[j];
+    for (let i = 0; i < selects.length; ++i) {
+        const select = selects[i];
+        for (let j = 0; j < bodyGroups.length; ++j) {
+            const group = bodyGroups[j];
             if (lvl === 0) {
-                var option = document.createElement('option');
+                const option = document.createElement('option');
                 option.value = group;
                 option.innerHTML = group;
                 select.appendChild(option);
             }
             else {
-                for (var k = 0; k < 3; ++k) {
-                    var val = group + k.toString();
-                    var option = document.createElement('option');
+                for (let k = 0; k < 3; ++k) {
+                    const val = group + k.toString();
+                    const option = document.createElement('option');
                     option.value = val;
                     option.innerHTML = val;
                     select.appendChild(option);
@@ -103,16 +103,16 @@ function levelChange() {
 function drawMonster() {
     ctx.fillRect(0, 0, canvasSize, canvasSize);
     const lvl = getLvl();
-    for (var i = 0; i < bodySelects.length; ++i) {
-        var bodySelect = bodySelects[i];
+    for (let i = 0; i < bodySelects.length; ++i) {
+        const bodySelect = bodySelects[i];
         if (bodySelect.disabled) {
             continue;
         }
-        var part = bodyParts[i];
-        var imageKey = lvl.toString() + "-" + part + "-" + bodySelect.options[bodySelect.selectedIndex].value;
-        var image = images[imageKey];
-        var posX = 0;
-        var posY = 0;
+        const part = bodyParts[i];
+        const imageKey = lvl.toString() + "-" + part + "-" + bodySelect.options[bodySelect.selectedIndex].value;
+        const image = images[imageKey];
+        let posX = 0;
+        let posY = 0;
         if (lvl !== 3) {
             posX = (canvasSize - smallSize) / 2;
             posY = canvasSize - smallSize;
