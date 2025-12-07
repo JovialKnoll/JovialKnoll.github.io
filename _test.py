d7d4258c8c34b134c5462e6344826849c1e2fdfe
Brett Kaplan <BrettCKaplan@gmail.com>
2025-12-06 20:32:46 -0500

    make build dir if needed

--- _test.py
+++ _test.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+import os
+import shutil
 import sys
 
+_location = '.'
+if __file__:
+    _location = __file__
+SRC_DIR = os.path.dirname(_location)
+BUILD_DIR = os.path.join(SRC_DIR, 'build')
 
+def main():
+    if not os.path.exists(BUILD_DIR):
+        os.makedirs(BUILD_DIR)
+
+if __name__ == '__main__':
+    main()
 
 sys.exit()
