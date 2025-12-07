a4f8ab80850ac973c9fae756ee0542dcffc4371c
Brett Kaplan <BrettCKaplan@gmail.com>
2025-12-06 20:47:30 -0500

    finixh initial copy

--- _test.py
+++ _test.py
@@ -11,8 +11,9 @@ SRC_DIR = os.path.dirname(_location)
 BUILD_DIR = os.path.join(SRC_DIR, 'build')
 
 def main():
-    if not os.path.exists(BUILD_DIR):
-        os.makedirs(BUILD_DIR)
+    if os.path.exists(BUILD_DIR):
+        shutil.rmtree(BUILD_DIR)
+    shutil.copytree(SRC_DIR, BUILD_DIR, ignore=shutil.ignore_patterns('.git*', '_*', 'CNAME'))
 
 if __name__ == '__main__':
     main()
