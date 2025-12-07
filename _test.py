ca4aa98f255916d74d7d2bf90986f9a48e53656c
Brett Kaplan <BrettCKaplan@gmail.com>
2025-12-06 21:31:32 -0500

    edit files as needed in 'build'

--- _test.py
+++ _test.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import fnmatch
 import sys
 
 _location = '.'
@@ -14,6 +15,14 @@ def main():
     if os.path.exists(BUILD_DIR):
         shutil.rmtree(BUILD_DIR)
     shutil.copytree(SRC_DIR, BUILD_DIR, ignore=shutil.ignore_patterns('.git*', '_*', 'CNAME'))
+    for dirpath, dirnames, filenames in os.walk(BUILD_DIR):
+        for filename in fnmatch.filter(filenames, '*.html') + fnmatch.filter(filenames, '*.js'):
+            filepath = os.path.join(dirpath, filename)
+            with open(filepath, 'r') as f:
+                text = f.read()
+            text = text.replace('="/', '="./')
+            with open(filepath, 'w') as f:
+                f.write(text)
 
 if __name__ == '__main__':
     main()
